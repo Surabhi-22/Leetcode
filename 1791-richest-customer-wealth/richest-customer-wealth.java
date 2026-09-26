0f4e03841886ebaa2e91c68b3f1b971d1382ac34
Surabhi Chourasiya <surabhichourasiya422@gmail.com>
2026-09-26 23:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (36.80%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+
+        int max = 0;
+
+        for(int i = 0; i < accounts.length; i++) {
+
+            int res = 0;
+
+            for(int j = 0; j < accounts[i].length; j++) {
+                res += accounts[i][j];
+            }
+
+            max = Math.max(max, res);
+        }
+
+        return max;
+    }
+}
